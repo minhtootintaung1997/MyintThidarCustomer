20c4c5f525f7df3d14a64b657756b45be8479bdd
minhtootintaung <minhtootintaungdev.mm@gmail.com>
2020-02-27 17:20:15 +0630

    hh

---
+++ Myint Thidar Customer/NotificationModel.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationModel.swift
+//  Myint Thidar Customer
+//
+//  Created by Min Htoo Tint Aung on 2/25/20.
+//  Copyright © 2020 Nanolabs. All rights reserved.
+//
+
+import UIKit
+class NotificationModel {
+    var title:String = ""
+    var date:String = ""
+    
+    
+    init(title:String,date:String) {
+        self.title=title
+        self.date=date
+    }
+}
